834100e2e577a0b9a07d56a78bd253b6cccb48ec
Alex Lednev <alexlednev@gmail.com>
2023-01-06 18:31:37 +0300

    section 2: basic routing, views, constraints, filesystem working, redirects, wildcards, caching

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat magna in orci vestibulum interdum. Vestibulum ut tincidunt elit. Nam imperdiet luctus sem, nec sagittis dui dignissim egestas. Proin id risus tincidunt, vehicula libero sed, efficitur mauris. Morbi nec justo nec felis ultrices egestas vitae non dolor. Praesent et enim quis risus ornare viverra nec vitae leo. Phasellus vehicula mi nec arcu pulvinar, ut consectetur dui iaculis. Cras nec cursus leo, sit amet tincidunt enim. Morbi mattis sem at dolor feugiat cursus at vel elit. Donec et facilisis elit, quis commodo ligula. Aenean dignissim finibus urna at ullamcorper. Nulla id metus pretium, malesuada orci a, varius ipsum.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat magna in orci vestibulum interdum. Vestibulum ut tincidunt elit. Nam imperdiet luctus sem, nec sagittis dui dignissim egestas. Proin id risus tincidunt, vehicula libero sed, efficitur mauris. Morbi nec justo nec felis ultrices egestas vitae non dolor. Praesent et enim quis risus ornare viverra nec vitae leo. Phasellus vehicula mi nec arcu pulvinar, ut consectetur dui iaculis. Cras nec cursus leo, sit amet tincidunt enim. Morbi mattis sem at dolor feugiat cursus at vel elit. Donec et facilisis elit, quis commodo ligula. Aenean dignissim finibus urna at ullamcorper. Nulla id metus pretium, malesuada orci a, varius ipsum.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat magna in orci vestibulum interdum. Vestibulum ut tincidunt elit. Nam imperdiet luctus sem, nec sagittis dui dignissim egestas. Proin id risus tincidunt, vehicula libero sed, efficitur mauris. Morbi nec justo nec felis ultrices egestas vitae non dolor. Praesent et enim quis risus ornare viverra nec vitae leo. Phasellus vehicula mi nec arcu pulvinar, ut consectetur dui iaculis. Cras nec cursus leo, sit amet tincidunt enim. Morbi mattis sem at dolor feugiat cursus at vel elit. Donec et facilisis elit, quis commodo ligula. Aenean dignissim finibus urna at ullamcorper. Nulla id metus pretium, malesuada orci a, varius ipsum.
+    </p>
+</article>
+
+</body>
+</html>
