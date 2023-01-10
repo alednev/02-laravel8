49af0cc34fcd9a8dd0bdfb44ae599372268f4609
Alex Lednev <alexlednev@gmail.com>
2023-01-10 22:26:02 +0300

    view all posts by an author "alias" of user, add a unique username for users,

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', action: function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get()
     ]);
 });
 
@@ -30,5 +31,11 @@ Route::get('posts/{post:slug}', action: function (Post $post) { // Post::where('
 Route::get('categories/{category:slug}', action: function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', action: function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
