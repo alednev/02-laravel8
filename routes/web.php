5a6d9c32805097ffe19a3ce78c47ecfd905652a8
Alex Lednev <alexlednev@gmail.com>
2023-01-25 15:22:14 +0300

    move from route file to new post controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -16,25 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', action: function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%')
-            ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
-
-    return view('posts', [
-        'posts' => $posts->get(),
-        'categories' => Category::all(),
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', action: function (Post $post) { // Post::where('slug', $post)->firstOrFail()
-    return view('post', [
-        'post' => $post,
-    ]);
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', action: function (Category $category) {
     return view('posts', [
