ad46f57486e6c04d8b72fa43a8c13d6d71c78339
Alex Lednev <alexlednev@gmail.com>
2023-01-25 16:52:44 +0300

    combine category in post filter method

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('categories/{category:slug}', action: function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all(),
-    ]);
-})->name('category');
-
 Route::get('authors/{author:username}', action: function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
