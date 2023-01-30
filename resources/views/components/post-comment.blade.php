28e98cca6a1cbff0032989913b594e7f00e848e5
Alex Lednev <alexlednev@gmail.com>
2023-01-30 16:32:01 +0300

    comments: table consistency, make foreign keys, create migration and factory, make a view component

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,21 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0 ">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Dow</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
-        <p>Laborum nihil dignissimos autem consequuntur quo. Facere voluptatum cum libero pariatur
-            enim cum. Voluptas modi sapiente a. Et voluptas omnis totam voluptatem accusamus eos.
-            Ut et aspernatur est officiis dolores delectus fuga. Nobis rem autem eius eveniet est
-            rerum minima. Est et beatae ut quo amet placeat et. Sequi natus dolorem quia voluptas
-            quidem distinctio.</p>
+        <p>
+            {{ $comment->body }}
+        </p>
     </div>
 </article>
